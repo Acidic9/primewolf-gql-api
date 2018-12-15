fc242a2d4c216b86a3b9756bec6d520dabf2d435
Ari Seyhun <ariseyhun@live.com.au>
2018-12-15 22:47:41 +1030

    Fix users query

---
+++ lib/primewolf_web/resolvers/user_resolver.ex
@@ -0,0 +1,24 @@
+defmodule PrimeWolfWeb.Resolvers.UserResolver do
+	
+	def users(_, _, _) do
+		{
+			:ok,
+			[
+				%{
+					id: "69",
+					first_name: "Ari",
+					last_name: "Seyhun",
+					email: "ariseyhun@live.com.au",
+					role: "admin"
+				},
+				%{
+					id: "42",
+					first_name: "Jimmy",
+					last_name: "Lee",
+					email: "jimmylee@gmail.com",
+					role: "user"
+				}
+			]
+		}
+	end
+end
--- lib/primewolf_web/schema.ex
+++ lib/primewolf_web/schema.ex
@@ -1,15 +1,24 @@
 defmodule PrimeWolfWeb.Schema do
   use Absinthe.Schema
+  alias PrimeWolfWeb.Resolvers
 
   # Import Types
   import_types(PrimeWolfWeb.Schema.Types)
 
   query do
+    @desc "Get a list of all users"
+    field(:users, list_of(:user_type)) do
+      # Resolver
+      resolve(&Resolvers.UserResolver.users/3)
+    end
   end
 
   # mutation do
   # end
 
   # subscription do
+  #   field :roulette_spin, :spin do
+
+  #   end
   # end
 end
