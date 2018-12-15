ff07e0f510971d3ad9b808590fa88c24e2a4e5e7
Ari Seyhun <ariseyhun@live.com.au>
2018-12-15 21:39:19 +1030

    Add absinthe routes

--- lib/primewolf_web/router.ex
+++ lib/primewolf_web/router.ex
@@ -5,7 +5,13 @@ defmodule PrimewolfWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", PrimewolfWeb do
+  scope "/api" do
     pipe_through :api
+
+    forward "/graphql", Absinthe.Plug, schema: PrimewolfWeb.Schema
+
+    if Mix.env == :dev do
+      forward "/graphiql", Absinthe.Plug.GraphiQL, schema: PrimewolfWeb.Schema
+    end
   end
 end
